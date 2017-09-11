cb56e84e87a89217b8c3e965929f307c5c29d2ed
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-09-11 19:20:22 +0300

    initial

---
+++ src/main/java/ClassB.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ivan on 11.09.2017.
+ */
+public class ClassB {
+
+    public void methodA() {
+
+    }
+
+    public void methodB() {
+
+    }
+
+    public void methodC() {
+
+    }
+}
---
+++ src/main/java/ClassC.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ivan on 11.09.2017.
+ */
+public class ClassC {
+
+    public void methodA() {
+
+    }
+
+    public void methodB() {
+
+    }
+
+    public void methodC() {
+
+    }
+}
